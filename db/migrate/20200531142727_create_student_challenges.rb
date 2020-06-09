a367059f903cb4481e8ab9145552997a2da17594
Bill Tihen <btihen@gmail.com>
2020-06-09 22:49:14 +0200

    add demo marker to activities

--- db/migrate/20200531142727_create_student_challenges.rb
+++ db/migrate/20200531142727_create_student_challenges.rb
@@ -1,8 +1,9 @@
 class CreateStudentChallenges < ActiveRecord::Migration[6.0]
   def change
     create_table :student_challenges do |t|
-      t.string     :student_challenge_name,  null: false
-      t.references :student_challenge_level, null: false, foreign_key: true
+      t.string     :student_challenge_name,    null: false
+      t.boolean    :student_challenge_is_demo, null: false, default: false
+      t.references :student_challenge_level,   null: false, foreign_key: true
 
       t.timestamps
 
--- db/migrate/20200601094051_create_faculty_challenges.rb
+++ db/migrate/20200601094051_create_faculty_challenges.rb
@@ -1,9 +1,10 @@
 class CreateFacultyChallenges < ActiveRecord::Migration[6.0]
   def change
     create_table :faculty_challenges do |t|
-      t.string :faculty_challenge_name
+      t.string     :faculty_challenge_name,      null: false
+      t.boolean    :faculty_development_is_demo, null: false, default: false
 
-      t.references :faculty_development_group, null: false, foreign_key: true
+      t.references :faculty_development_group,   null: false, foreign_key: true
 
       t.timestamps
     end
